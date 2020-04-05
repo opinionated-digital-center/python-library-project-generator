707232dff11a448b651ab1225a0d716af2a206dd
Sylvain LEQUEUX <slequeux@gmail.com>
2020-04-05 17:26:19 +0200

    feat(github-actions): try using an action to provision environment

---
+++ .github/actions/provision-project-from-cookiecutter/instanciate_project_and_validate_it.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+# Prepare directories
+if [ ! -z $1 ]; then
+  COOKIE_DIR=$1
+else
+  COOKIE_DIR=$PWD
+fi
+
+mkdir -p /projects
+cd /projects
+
+# Instanciate project
+cookiecutter --default-config --no-input ${COOKIE_DIR}
+cd python_boilerplate
+
+# Install dev env
+make setup-dev-host
+source $HOME/.bashrc
+make setup-dev-env-minimal
+
+# Build project
+make tox
