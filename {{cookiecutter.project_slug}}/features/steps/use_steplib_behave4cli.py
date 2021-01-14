8357c321f2470db3f69929f37e8a7b33e68747ce
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2021-01-14 23:20:27 +0100

    feat: update dependencies and improved scripts from other project's experience

--- {{cookiecutter.project_slug}}/features/steps/use_steplib_behave4cli.py
+++ {{cookiecutter.project_slug}}/features/steps/use_steplib_behave4cli.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
+# flake8: noqa
 """
 Use behave4cli step library.
 """
 
-from __future__ import absolute_import
-
 # -- REGISTER-STEPS FROM STEP-LIBRARY:
 import behave4cli.__all_steps__
-import behave4cli.failing_steps
-import behave4cli.note_steps
-import behave4cli.passing_steps  # noqa: F401
